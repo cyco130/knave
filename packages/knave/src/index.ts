7602558d6bcdc211bfd4340db964a82c2439f4d0
cyco130 <cyco130@gmail.com>
2022-09-26 08:17:43 +0300

    fix: honor meta key on link click

--- packages/knave/src/index.ts
+++ packages/knave/src/index.ts
@@ -59,6 +59,7 @@ export interface MouseEventLike {
 	shiftKey: boolean;
 	altKey: boolean;
 	ctrlKey: boolean;
+	metaKey: boolean;
 
 	preventDefault(): void;
 }
@@ -84,6 +85,7 @@ export function shouldHandleClick(e: MouseEventLike): boolean {
 		!e.shiftKey &&
 		!e.altKey &&
 		!e.ctrlKey &&
+		!e.metaKey &&
 		(!t.target || t.target !== "_self") &&
 		!t.hasAttribute("download") &&
 		!t.relList.contains("external")
